37c92f5e55e10da317512482690adb2eed39787e
shantocnits <shanto.cnits@gmail.com>
2024-05-30 15:43:26 +0600

    fullscreen fix

---
+++ assets/js/full-screen-toggle.js
@@ -0,0 +1,66 @@
+if(document.fullscreenEnabled || document.webkitFullscreenEnabled) {
+	const toggleBtn = document.querySelector('.js-toggle-fullscreen-btn');
+
+	const styleEl = document.createElement('link');
+	styleEl.setAttribute('rel', 'stylesheet');
+	styleEl.setAttribute('href', 'https://codepen.io/tiggr/pen/poJoLyW.css');
+	styleEl.addEventListener('load', function() {
+		toggleBtn.hidden = false;
+	});
+	document.head.appendChild(styleEl);
+	
+	toggleBtn.addEventListener('click', function() {
+		if(document.fullscreen) {
+			document.exitFullscreen();
+		} else if(document.webkitFullscreenElement) {
+			document.webkitCancelFullScreen()
+		} else if(document.documentElement.requestFullscreen) {
+			document.documentElement.requestFullscreen();
+		} else {
+			document.documentElement.webkitRequestFullScreen();
+		}
+	});
+	
+	document.addEventListener('fullscreenchange', handleFullscreen);
+	document.addEventListener('webkitfullscreenchange', handleFullscreen);
+	
+	
+	function handleFullscreen() {
+		if(document.fullscreen || document.webkitFullscreenElement) {
+			toggleBtn.classList.add('on');
+			toggleBtn.setAttribute('aria-label', 'Exit fullscreen mode');
+		} else {
+			toggleBtn.classList.remove('on');
+			toggleBtn.setAttribute('aria-label', 'Enter fullscreen mode');
+		}
+	}
+}
+
+// full screen end
+
+
+// dark mode start
+
+
+function toggle_light_mode() {
+    var app = document.getElementsByTagName("BODY")[0];
+    if (localStorage.lightMode == "dark") {
+        localStorage.lightMode = "light";
+        app.setAttribute("light-mode", "light");
+    } else {
+        localStorage.lightMode = "dark";
+        app.setAttribute("light-mode", "dark");
+    }
+}
+
+window.addEventListener(
+    "storage",
+    function () {
+        if (localStorage.lightMode == "dark") {
+            app.setAttribute("light-mode", "dark");
+        } else {
+            app.setAttribute("light-mode", "light");
+        }
+    },
+    false
+);
