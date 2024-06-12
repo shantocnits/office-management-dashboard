62ca9a03a236eb0c92925cfdd23e15c7aed3335b
shantocnits <shanto.cnits@gmail.com>
2024-06-12 15:06:59 +0600

    table ongoing

---
+++ assets/js/earning-pie-chart.js
@@ -0,0 +1,20 @@
+var options = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+    type: 'donut',
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+  };
+
+  var chart = new ApexCharts(document.querySelector("#piechart"), options);
+  chart.render();
